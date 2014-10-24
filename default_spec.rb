5f273e858119c9134c4f86c3385432138c4c2f7e
koduki <koduki@nebula>
2014-10-25 02:35:38 +0900

    make spec.

---
+++ default_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe 'common' do
+  # check os version
+  describe file('/etc/redhat-release') do
+    it { should contain 'CentOS Linux release 7.0' }
+  end
+
+  # check hostname
+  describe file('/etc/sysconfig/network') do
+    it { should contain '#{property[:host_name]}' }
+  end
+
+  # check packages
+  describe_packages[
+    "rsync",
+    "unzip",
+    "telnet"
+  ]
+end
